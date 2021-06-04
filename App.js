af051a79665db9f9199db6a9b7ef87504d718a7d
Ousmane Diop <ousmanediopp268@gmail.com>
2021-06-04 18:41:48 +0000

    change files

--- App.js
+++ App.js
@@ -11,17 +11,16 @@ import React, {useState} from 'react';
 import {StyleSheet} from 'react-native';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
-import AuthContext from './auth/context';
-import storageService from './auth/storageService';
-import AppNavigator from './navigation/AppNavigator';
-import AuthNavigator from './navigation/AuthNavigator';
-import navigationTheme from './navigation/navigationTheme';
-import {navigationRef} from './navigation/rootNavigation';
-import store from './store/configureStore';
+import AuthContext from './src/auth/context';
+import storageService from './src/auth/storageService';
+import AppNavigator from './src/navigation/AppNavigator';
+import AuthNavigator from './src/navigation/AuthNavigator';
+import navigationTheme from './src/navigation/navigationTheme';
+import {navigationRef} from './src/navigation/rootNavigation';
+import store from './src/store/configureStore';
 
 const App: () => Node = () => {
   const [user, setUser] = useState(false);
-  const [isReady, setIsReady] = useState(false);
 
   const restoreUser = async () => {
     const user = await storageService.getData();
